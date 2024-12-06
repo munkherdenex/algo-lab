0893074a6e4749d16414344189e5d7d69b6feae6
munkherdenex <munkherdenex@gmail.com>
2024-12-06 19:06:24 +0800

    feat: lab 11

---
+++ Lab_daalgavar/lab_11.py
@@ -0,0 +1,25 @@
+class Solution:
+    def checkValidString(self, s: str) -> bool:
+        low = 0 
+        high = 0  
+        
+        for char in s:
+            if char == '(':
+                low += 1
+                high += 1
+            elif char == ')':
+                low = max(low - 1, 0)  
+                high -= 1
+            elif char == '*':
+                low = max(low - 1, 0)  
+                high += 1  
+
+            if high < 0:
+                return False
+        
+        return low == 0
+
+solution = Solution()
+print(solution.checkValidString("()"))   
+print(solution.checkValidString("(*)"))     
+print(solution.checkValidString("(*))"))    
